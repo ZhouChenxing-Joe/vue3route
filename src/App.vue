6ddcf2244a4fc38901630c107ff1d8914e567207
ZhouChenxing-Joe <w86311525@gmail.com>
2024-12-09 15:12:03 +0800

    version 1

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <Person/>
+</template>
+
+
+<!-- setup的语法糖 在script里直接写setup就不用在script里写setup代码了 -->
+ <!-- 需要在Terminal里面安装这个npm i vite-plugin-vue-setup-extend -->
+  <!-- 然后再vite.config.ts里import npm i vite-plugin-vue-setup-extend -->
+<script lang="ts" setup name="App">
+import Person from './components/Person.vue';
+import {ref,reactive} from 'vue'
+</script>
+
+<style>
+</style>
