b23016b9be1dd6d5342f376edf2dc90525346658
ZhouChenxing-Joe <w86311525@gmail.com>
2024-12-10 19:54:10 +0800

    version 2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-    <Person/>
+  <div class=" app">
+    <h2>Vue路由测试</h2>
+    <!-- 导航区 -->
+     <div class="navigate">
+      <a href="#">首页</a>
+      <a href="#">新闻</a>
+      <a href="#">关于</a>
+     </div>
+     <!-- 展示区 -->
+      <div class="main-content">
+         此处以后可能展示各种组件到底展示那个需要看路径
+      </div>
+  </div>
 </template>
 
 
@@ -7,9 +19,53 @@
  <!-- 需要在Terminal里面安装这个npm i vite-plugin-vue-setup-extend -->
   <!-- 然后再vite.config.ts里import npm i vite-plugin-vue-setup-extend -->
 <script lang="ts" setup name="App">
-import Person from './components/Person.vue';
-import {ref,reactive} from 'vue'
 </script>
 
 <style>
+/* App */
+.title{
+  text-align: center;
+  word-spacing: 5px;
+  margin: 30px 0;
+  height: 70px;
+  line-height: 70px;
+  background-image: linear-gradient(45deg,gray,white);
+  border-radius: 10px;
+  box-shadow: 0 0 2px;
+  font-size: 30px;
+}
+.navigate{
+  display: flex;
+  justify-content: space-around;
+  margin: 0 100px;
+}
+.navigate a {
+  display: block;
+  text-align:  center;
+  width: 90px;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 10px;
+  background-color: gray;
+  text-decoration: none;
+  color: white;
+  font-size: 18px;
+  letter-spacing: 5px;
+}
+.navigate a.active{
+  background-color: #64767E;
+  color: #ffc268;
+  font-weight: 900;
+  text-shadow: 0 0 1px black;
+  font-family: 微软雅黑;
+}
+.main-content{
+  margin: 0 auto;
+  margin-top: 30px;
+  border-radius: 10px;
+  width: 90%;
+  height: 400px;
+  border: 1px solid;
+}
+
 </style>
