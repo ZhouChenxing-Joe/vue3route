ec32fd0214061d1deb33660d89f77854c1bf6c93
ZhouChenxing-Joe <w86311525@gmail.com>
2024-12-19 19:38:13 +0800

    version 20

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="count">
         <h2>当前求和为: {{ sum }}</h2>
-        <select v-model="n">
+        <select v-model.number="n">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -38,6 +38,6 @@ function minus(){
 }
 select,button{
     margin: 0 5px;
-    height: 20px;
+    height: 25px;
 }
 </style>
