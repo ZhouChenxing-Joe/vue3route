976bc63c9c9eacc4999f2fbb956b6493218e1417
ZhouChenxing-Joe <w86311525@gmail.com>
2024-12-27 18:34:31 +0800

    version 47

--- src/pages/04_v-model/Father.vue
+++ src/pages/04_v-model/Father.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="father">
     <h3>父组件</h3>
+    <!-- v-model 用在html标签上 -->
+    <!-- <input type="text" v-model="username"> -->
+     
+    <!-- 下面这行代码是第五行代码的底层写法 -->
+     <!-- <input type="text" :value="username" @input="username = (<HTMLInputElement>$event.target).value"> -->
+      
+      <!-- v-model 用在组件标签上  -->
+      <!-- <JojoInput v-model="username"/> -->
+      <JojoInput 
+      :modelValue="username" 
+      @update:modelValue ="username = $event"/>
   </div>
 </template>
 
 <script setup lang="ts" name="Father">
 	import { ref } from "vue";
+  import JojoInput from "./JojoInput.vue";
+
+  let username = ref('Joe chenxing')
 
 </script>
 
---
+++ src/pages/04_v-model/JojoInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <input type="text" :value="modelValue">
+</template>
+
+<script setup lang="ts" name="JojoInput">
+    defineProps(['modelValue'])
+</script>
+
+<style scoped>
+    input{
+        border: 2px solid greenyellow;
+        background-image: linear-gradient(100deg,gray,green,greenyellow);
+        height: 30px;
+        font-size: 18px;
+        color: white;
+    }
+</style>
